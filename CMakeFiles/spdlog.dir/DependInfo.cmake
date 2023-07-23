a1a4a339632bcd9a27849520562068186ec064b4
Ashish Utagikar <aputagikar@gmail.com>
2023-07-23 23:25:02 +0000

    Second Revision

---
+++ CMakeFiles/spdlog.dir/DependInfo.cmake
@@ -0,0 +1,32 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/ubuntu/workarea/spdlog/src/async.cpp" "/home/ubuntu/workarea/spdlog/CMakeFiles/spdlog.dir/src/async.cpp.o"
+  "/home/ubuntu/workarea/spdlog/src/bundled_fmtlib_format.cpp" "/home/ubuntu/workarea/spdlog/CMakeFiles/spdlog.dir/src/bundled_fmtlib_format.cpp.o"
+  "/home/ubuntu/workarea/spdlog/src/cfg.cpp" "/home/ubuntu/workarea/spdlog/CMakeFiles/spdlog.dir/src/cfg.cpp.o"
+  "/home/ubuntu/workarea/spdlog/src/color_sinks.cpp" "/home/ubuntu/workarea/spdlog/CMakeFiles/spdlog.dir/src/color_sinks.cpp.o"
+  "/home/ubuntu/workarea/spdlog/src/file_sinks.cpp" "/home/ubuntu/workarea/spdlog/CMakeFiles/spdlog.dir/src/file_sinks.cpp.o"
+  "/home/ubuntu/workarea/spdlog/src/spdlog.cpp" "/home/ubuntu/workarea/spdlog/CMakeFiles/spdlog.dir/src/spdlog.cpp.o"
+  "/home/ubuntu/workarea/spdlog/src/stdout_sinks.cpp" "/home/ubuntu/workarea/spdlog/CMakeFiles/spdlog.dir/src/stdout_sinks.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "SPDLOG_COMPILED_LIB"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
